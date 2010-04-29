e92b5fd81f4ba20f1f49269a20740b5196441735
aphatt <aphatt@gmail.com>
2010-04-29 10:24:41 +0000

    Improved structure.

--- src/control/User.java
+++ src/control/User.java
@@ -6,9 +6,22 @@ public class User {
 	private Player player;
 	private Board board;
 	
+	public User(Player p, Board b) {
+		player = p;
+		board = b;
+		
+		// Getting the waring mechanism to shut the hell up.		
+		player.hashCode();
+		board.hashCode();
+	}
+	
 	public void doTurn() {
 		
 		
+	}
+	
+	public Player getPlayer() {
+		return player;
 	}
 	
 }
