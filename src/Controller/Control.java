08b0380211ed648811292b6078d0b584d3d5598e
bechri88 <bechri88@07d7e391-7852-3798-70c1-28dc30431642>
2010-04-29 11:23:16 +0000

    New Game and MainFrame merge

---
+++ src/Controller/Control.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import Model.Player;
+
+public class Control {
+
+	/*private Player activePlayer;
+	private static final Control INSTANCE = new Control();
+	
+	private Control(){
+    }
+	
+	public static Control getInstance(){
+		return INSTANCE;
+	}
+	
+	public Player getActivePlayer(){
+		activePlayer = GameState.getActivePlayer();
+	}
+	
+	public boolean placePiece(int x, int y){
+		Board.place(x,y,getActivePlayer());
+	}
+	
+	public boolean setVisible(){
+		return true:
+	}*/
+}
