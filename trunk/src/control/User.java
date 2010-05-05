a66c35716edabe4d4783fd05296f3a5d253cffd0
aphatt@gmail.com <aphatt@gmail.com@07d7e391-7852-3798-70c1-28dc30431642>
2010-05-05 13:10:25 +0000

    Three old report we had forgotten to upload.

--- trunk/src/control/User.java
+++ trunk/src/control/User.java
@@ -5,15 +5,10 @@ public abstract class User {
 	
 	private Player player;
 	
-	public User(Player p) {
-		player = p;
-	}
-	
+	public User(Player p) {	player = p; }	
 	public Player getPlayer() { return player; }
 	protected void setPlayer(Player p) { player = p; }
 	public abstract void doTurn(Board b);
 	public abstract String getNotice();
 	public abstract void disconnect();
-	
-
 }
