630bdb4b26767502dc23abd3829145b618963ebd
bechri88 <bechri88@07d7e391-7852-3798-70c1-28dc30431642>
2010-05-10 16:26:06 +0000

    Updated AIUser

--- trunk/src/control/AIUser.java
+++ trunk/src/control/AIUser.java
@@ -1,5 +1,6 @@
 package control;
 
+import java.awt.Point;
 import java.util.Arrays;
 
 import model.Board;
@@ -7,90 +8,133 @@ import model.Player;
 
 public class AIUser extends User {
 
-	private boolean [][] possibleWinZ;
-	private boolean [][][] possibleWinX;
-	private boolean [][][] possibleWinY;
+	private boolean [][] defensivePossibleWinZ;
+	private boolean [][][] defensivePossibleWinX;
+	private boolean [][][] defensivePossibleWinY;
+	private boolean [][] offensivePossibleWinZ;
+	private boolean [][][] offensivePossibleWinX;
+	private boolean [][][] offensivePossibleWinY;
 	private boolean hasPlaced;
 	private int count = 0;
 	private Player AI;
-	
+	private Point lastMove;
+
 	public AIUser(Player p) {
 		super(p);
-		
+
 	}
 
 	public void doTurn(Board b) {
 		AI = getPlayer();
 		hasPlaced = false;
 		if(count == 0){
-			possibleWinZ = new boolean [b.getRows()][b.getColumns()];
-			possibleWinX = new boolean [b.getRows()][b.getColumns()][b.getLayers()];
-			possibleWinY = new boolean [b.getRows()][b.getColumns()][b.getLayers()];
+			defensivePossibleWinZ = new boolean [b.getRows()][b.getColumns()];
+			defensivePossibleWinX = new boolean [b.getRows()][b.getColumns()][b.getLayers()];
+			defensivePossibleWinY = new boolean [b.getRows()][b.getColumns()][b.getLayers()];
+			offensivePossibleWinZ = new boolean [b.getRows()][b.getColumns()];
+			offensivePossibleWinX = new boolean [b.getRows()][b.getColumns()][b.getLayers()];
+			offensivePossibleWinY = new boolean [b.getRows()][b.getColumns()][b.getLayers()];
 			for(int i = 0; i<b.getRows(); i++){
 				for(int j = 0; j<b.getColumns(); j++){
-					possibleWinZ[i][j] = true;
-						for(int k = 0; k < b.getLayers(); k++){
-							possibleWinX[i][j][k] = true;
-							possibleWinY[i][j][k] = true;
-						}
+					defensivePossibleWinZ[i][j] = true;
+					offensivePossibleWinZ[i][j] = true;
+					for(int k = 0; k < b.getLayers(); k++){
+						defensivePossibleWinX[i][j][k] = true;
+						defensivePossibleWinY[i][j][k] = true;
+						offensivePossibleWinX[i][j][k] = true;
+						offensivePossibleWinY[i][j][k] = true;
+
+					}
 				}
 			}
 		}
 		count++;
-		
+
 		/* =========================================================================== //
 		 						Defensive calculations
 		// =========================================================================== */
-		
+
+		for(int i = 0; i < b.getRows(); i++){
+			for(int j = 0; j < b.getColumns()-1; j++){
+				System.out.print(defensivePossibleWinX[i][j][0]+"	|	");
+			}
+			System.out.println(defensivePossibleWinX[i][b.getColumns()-1][0]);
+		}
+		System.out.println("==================================");
+		for(int i = 0; i < b.getRows(); i++){
+			for(int j = 0; j < b.getColumns()-1; j++){
+				System.out.print(defensivePossibleWinY[i][j][0]+"	|	");
+			}
+			System.out.println(defensivePossibleWinY[i][b.getColumns()-1][0]);
+		}
+
+
 		int defensiveStartXinXDirection = 0;
 		int defensiveCurrentY = 0;
 		int defensiveMaxBallsInARowX = 0;
 		int defensiveCurrentZinXDirection = 0;
+		boolean possibleToWinY = true;
 		for(int x = 0; x < b.getRows(); x++){
 			for(int z = 0; z < b.getLayers(); z++){
 				int allYourBaseAreBelongToUs = 0;
 				for(int y = 0; y < b.getColumns(); y++){
-					defensiveCurrentY = y;
 					if(b.peek(x,y,z) != null && !b.peek(x,y,z).equals(getPlayer())){
 						allYourBaseAreBelongToUs++;
+						defensiveCurrentY = y;
+					}
+					else{
+						if(x!=b.getRows()-1 && possibleToWinY && allYourBaseAreBelongToUs > defensiveMaxBallsInARowX){
+							if(countHeight(b,x,y)!=z && b.peek(x, y, z) == null && z!=0){
+								possibleToWinY = false;
+							}
+						}
 					}
 				}
-				if(possibleWinX[x][defensiveCurrentY][z] && defensiveMaxBallsInARowX < allYourBaseAreBelongToUs){
+				if(defensivePossibleWinX[x][defensiveCurrentY][z] && defensiveMaxBallsInARowX < allYourBaseAreBelongToUs && possibleToWinY){
 					defensiveMaxBallsInARowX = allYourBaseAreBelongToUs;
 					defensiveStartXinXDirection = x;
 					defensiveCurrentZinXDirection = z;
 				}
-				System.out.println("< Turn: " + count +" x > "+ defensiveMaxBallsInARowX +":" + allYourBaseAreBelongToUs+ " cords: " +x+", "+ z );
+				//System.out.println("< Turn: " + count +" x > "+ defensiveMaxBallsInARowX +":" + allYourBaseAreBelongToUs+ " cords: " +x+", "+ z );
 			}
 		}
 		int defensiveStartYinYDirection = 0;
 		int defensiveMaxBallsInARowY = 0;
 		int defensiveCurrentX = 0;
 		int defensiveCurrentZinYDirection = 0;
+		boolean possibleToWinX = true;
 		for(int y = 0; y < b.getColumns(); y++){
 			for(int z = 0; z < b.getLayers(); z++){
 				int allYourBaseAreBelongToUs = 0;
 				for(int x = 0; x < b.getRows(); x++){
-					defensiveCurrentX = x;
+
 					if(b.peek(x,y,z) != null && !b.peek(x,y,z).equals(getPlayer())){
 						allYourBaseAreBelongToUs++;
+						defensiveCurrentX = x;
+					}
+					else{
+						if(x!=b.getRows()-1 && possibleToWinX && allYourBaseAreBelongToUs > defensiveMaxBallsInARowY){
+							if(countHeight(b,x,y)!=z && b.peek(x, y, z) == null && z!=0){
+								possibleToWinX = false;
+							}
+						}
 					}
 				}
-				
-				if(possibleWinY[defensiveCurrentX][y][z] && defensiveMaxBallsInARowY < allYourBaseAreBelongToUs){
+
+				if(defensivePossibleWinY[defensiveCurrentX][y][z] && defensiveMaxBallsInARowY < allYourBaseAreBelongToUs && possibleToWinX){
 					defensiveMaxBallsInARowY = allYourBaseAreBelongToUs;
 					defensiveStartYinYDirection = y;
 					defensiveCurrentZinYDirection = z;
 				}
-				System.out.println("< Turn: " + count +"y > "+ defensiveMaxBallsInARowY+":"+allYourBaseAreBelongToUs + " cords: " +y+", "+ z );
+				//System.out.println("< Turn: " + count +"y > "+ defensiveMaxBallsInARowY+":"+allYourBaseAreBelongToUs + " cords: " +y+", "+ z );
 			}
-			
+
 		}
-		
+
 		int defensiveMaxBallsInaRowZ = 0;
 		int defensiveStartYinZDirection = 0;
 		int defensiveStartXinZDirection = 0;
-		
+
 		for(int x = 0; x<b.getRows(); x++){
 			int maxZ;
 			for(int y = 0; y<b.getColumns(); y++){
@@ -99,21 +143,56 @@ public class AIUser extends User {
 					if(b.peek(x, y, z)!= null && !b.peek(x, y, z).equals(getPlayer())){
 						maxZ++;
 					}
-					
+
+
 				}
-				if(possibleWinZ[x][y] && maxZ > defensiveMaxBallsInaRowZ){
+				if(defensivePossibleWinZ[x][y] && maxZ > defensiveMaxBallsInaRowZ){
 					defensiveMaxBallsInaRowZ = maxZ;
 					defensiveStartYinZDirection = y;
 					defensiveStartXinZDirection = x;
 				}
 			}
 		}
-		
+
+
 		/* =========================================================================== //
 								Offensive calculations
 		// =========================================================================== */
-		
-		int offsensiveStartXinXDirection = 0;
+
+		lastMove = b.getLastMove();
+
+
+		for(int row = 0; row < b.getRows(); row++){
+			offensivePossibleWinY[row][lastMove.y][countHeight(b,lastMove.x,lastMove.y)-1] = false;
+		}
+		for(int column = 0; column < b.getColumns(); column++){
+			offensivePossibleWinX[lastMove.x][column][countHeight(b,lastMove.x,lastMove.y)-1] = false;;
+		}
+		offensivePossibleWinZ[lastMove.x][lastMove.y] = false;
+
+
+		/*boolean columnWithNoBalls = false;
+		boolean rowWithNoBalls    = false;
+		for(int i = 0; i < b.getRows(); i++){
+			boolean innerCheckRow    = true;
+			boolean innerCheckColumn = true;
+			for(int j = 0; j < b.getColumns()-1; j++){
+				if(offensivePossibleWinX[i][j][0]==false && innerCheckRow){
+					innerCheckRow = false;
+				}
+				if(offensivePossibleWinY[j][i][0]){
+					innerCheckColumn = false;
+				}
+			}
+			if(innerCheckRow)
+				rowWithNoBalls = true;
+
+			if(innerCheckColumn)
+				columnWithNoBalls = true;
+		}*/
+
+		// Räknar vertikalt sett från PinArea
+		int offensiveStartXinXDirection = 0;
 		int offensiveCurrentY = 0;
 		int offensiveMaxBallsInARowX = 0;
 		int offensiveCurrentZinXDirection = 0;
@@ -121,19 +200,24 @@ public class AIUser extends User {
 			for(int z = 0; z < b.getLayers(); z++){
 				int allYourBaseAreBelongToUs = 0;
 				for(int y = 0; y < b.getColumns(); y++){
-					offensiveCurrentY = y;
-					if(b.peek(x,y,z) != null && !b.peek(x,y,z).equals(getPlayer())){
+
+					if(b.peek(x,y,z) != null && b.peek(x,y,z).equals(getPlayer())){
 						allYourBaseAreBelongToUs++;
+						offensiveCurrentY = y;
+						System.out.println("< Turn: " + count +"offensive x > "+ offensiveMaxBallsInARowX +":" + allYourBaseAreBelongToUs+ " cords: " +x+", "+ offensiveCurrentY+", "+ z );
 					}
 				}
-				if(possibleWinX[x][offensiveCurrentY][z] && offensiveMaxBallsInARowX < allYourBaseAreBelongToUs){
+				if(offensivePossibleWinX[x][offensiveCurrentY][z] && offensiveMaxBallsInARowX < allYourBaseAreBelongToUs){
 					offensiveMaxBallsInARowX = allYourBaseAreBelongToUs;
-					offsensiveStartXinXDirection = x;
+					offensiveStartXinXDirection = x;
 					offensiveCurrentZinXDirection = z;
 				}
-				System.out.println("< Turn: " + count +" x > "+ offensiveMaxBallsInARowX +":" + allYourBaseAreBelongToUs+ " cords: " +x+", "+ z );
+
 			}
 		}
+
+
+		//Räknar rakt ner sett från PinArea
 		int offensiveStartYinYDirection = 0;
 		int offensiveMaxBallsInARowY = 0;
 		int offensiveCurrentX = 0;
@@ -142,114 +226,200 @@ public class AIUser extends User {
 			for(int z = 0; z < b.getLayers(); z++){
 				int allYourBaseAreBelongToUs = 0;
 				for(int x = 0; x < b.getRows(); x++){
-					offensiveCurrentX = x;
-					if(b.peek(x,y,z) != null && !b.peek(x,y,z).equals(getPlayer())){
+					if(b.peek(x,y,z) != null && b.peek(x,y,z).equals(getPlayer())){
 						allYourBaseAreBelongToUs++;
+						offensiveCurrentX = x;
+						//System.out.println("< Turn: " + count +"offensive y > "+ offensiveMaxBallsInARowY+":"+allYourBaseAreBelongToUs + " cords: "+ offensiveCurrentX+", "+y+", "+ z );
 					}
 				}
-				
-				if(possibleWinY[offensiveCurrentX][y][z] && offensiveMaxBallsInARowY < allYourBaseAreBelongToUs){
+
+				if(offensivePossibleWinY[offensiveCurrentX][y][z] && offensiveMaxBallsInARowY < allYourBaseAreBelongToUs){
 					offensiveMaxBallsInARowY = allYourBaseAreBelongToUs;
 					offensiveStartYinYDirection = y;
 					offensiveCurrentZinYDirection = z;
 				}
-				System.out.println("< Turn: " + count +"y > "+ offensiveMaxBallsInARowY+":"+allYourBaseAreBelongToUs + " cords: " +y+", "+ z );
+				//System.out.println("< Turn: " + count +"offensive y > "+ offensiveMaxBallsInARowY+":"+allYourBaseAreBelongToUs + " cords: "+ offensiveCurrentX+", "+y+", "+ z );
 			}
-			
+
 		}
-		
+
 		int offensiveMaxBallsInaRowZ = 0;
 		int offensiveStartYinZDirection = 0;
 		int offensiveStartXinZDirection = 0;
-		
+
 		for(int x = 0; x<b.getRows(); x++){
 			int maxZ;
 			for(int y = 0; y<b.getColumns(); y++){
 				maxZ = 0;
 				for(int z = 0; z<b.getLayers(); z++){
-					if(b.peek(x, y, z)!= null && !b.peek(x, y, z).equals(getPlayer())){
+					if(b.peek(x, y, z)!= null && b.peek(x, y, z).equals(getPlayer())){
 						maxZ++;
 					}
-					
+
 				}
-				if(possibleWinZ[x][y] && maxZ > offensiveMaxBallsInaRowZ){
+				if(offensivePossibleWinZ[x][y] && maxZ > offensiveMaxBallsInaRowZ){
 					offensiveMaxBallsInaRowZ = maxZ;
 					offensiveStartYinZDirection = y;
 					offensiveStartXinZDirection = x;
 				}
 			}
 		}
-		
+
+
 		/* =========================================================================== //
 					Where the AI should put the balls, defensive calculations
 		// =========================================================================== */
-		
-		if(defensiveMaxBallsInARowX > (b.getRows()-3) || offensiveMaxBallsInARowY > (b.getColumns()-3) || offensiveMaxBallsInaRowZ > (b.getRows()-2)){	
-			if(defensiveMaxBallsInARowX >= offensiveMaxBallsInARowY){
-				if(defensiveMaxBallsInARowX >= offensiveMaxBallsInaRowZ){
+
+		if((offensiveMaxBallsInARowX != (b.getRows()-1) && offensiveMaxBallsInARowY != (b.getColumns()-1) && offensiveMaxBallsInaRowZ != (b.getLayers())-1) && 
+				(defensiveMaxBallsInARowX > (b.getRows()-3) || defensiveMaxBallsInARowY > (b.getColumns()-3) || defensiveMaxBallsInaRowZ > (b.getRows()-2))){	
+			if(defensiveMaxBallsInARowX >= defensiveMaxBallsInARowY){
+				if(defensiveMaxBallsInARowX >= defensiveMaxBallsInaRowZ){
 					System.out.println("printar i x");
+					int xcurrenty = 0;
 					for(int y = 0; y < b.getColumns(); y++){
 						if(b.peek(defensiveStartXinXDirection,y,defensiveCurrentZinXDirection) == null){
 							b.place(defensiveStartXinXDirection, y, getPlayer());
 							hasPlaced = true;
+							xcurrenty = y;
 							break;
 						}
-						
+
 					}
-					
+
 					if(hasPlaced){
 						for(int y = 0; y<b.getColumns();y++){
-							possibleWinX[defensiveStartXinXDirection][y][defensiveCurrentZinXDirection]= false;
-							possibleWinY[y][defensiveStartXinXDirection][defensiveCurrentZinXDirection] = false;
+							defensivePossibleWinX[defensiveStartXinXDirection][y][defensiveCurrentZinXDirection]= false;
+							defensivePossibleWinY[y][xcurrenty][defensiveCurrentZinXDirection] = false;
 						}
-						possibleWinZ[defensiveStartXinXDirection][defensiveCurrentZinXDirection] = false;
+						defensivePossibleWinZ[defensiveStartXinXDirection][defensiveCurrentZinXDirection] = false;
 						return;
 					}
 				}
 				else{
-					if(b.place(offensiveStartXinZDirection, offensiveStartYinZDirection, getPlayer())){
-						System.out.println(possibleWinZ[offensiveStartYinYDirection][offensiveCurrentZinYDirection]);
-						possibleWinZ[offensiveStartXinZDirection][offensiveStartYinZDirection] = false;
+					if(b.place(defensiveStartXinZDirection, defensiveStartYinZDirection, getPlayer())){
+						System.out.println(defensivePossibleWinZ[defensiveStartYinYDirection][defensiveCurrentZinYDirection]);
+						defensivePossibleWinZ[defensiveStartXinZDirection][defensiveStartYinZDirection] = false;
 						return;
 					}
 				}
 			}
 			else{
-				if(offensiveMaxBallsInARowY >= offensiveMaxBallsInaRowZ){
-					System.out.println("printar i y, " + offensiveMaxBallsInARowY);
+
+				if(defensiveMaxBallsInARowY >= defensiveMaxBallsInaRowZ){
+					System.out.println("printar i y, " + defensiveMaxBallsInARowY);
+					int ycurrentx = 0;
 					for(int x = 0; x < b.getRows(); x++){
-						if(b.peek(x,offensiveStartYinYDirection,offensiveCurrentZinYDirection) == null){					
-							b.place(x, offensiveStartYinYDirection, getPlayer());
+						if(b.peek(x,defensiveStartYinYDirection,defensiveCurrentZinYDirection) == null){					
+							b.place(x, defensiveStartYinYDirection, getPlayer());
 							hasPlaced = true;
+							ycurrentx = x;
 							break;	
 						}
 					}
 					if(hasPlaced){
 						for (int x = 0; x < b.getRows(); x++) {
-							possibleWinY[x][offensiveStartYinYDirection][offensiveCurrentZinYDirection]= false;
-							possibleWinX[offensiveStartYinYDirection][x][offensiveCurrentZinYDirection] = false;
+							defensivePossibleWinY[x][defensiveStartYinYDirection][defensiveCurrentZinYDirection]= false;
+							defensivePossibleWinX[ycurrentx][x][defensiveCurrentZinYDirection] = false;
 						}
-						possibleWinZ[offensiveStartYinYDirection][offensiveCurrentZinYDirection] = false;
+						defensivePossibleWinZ[defensiveStartYinYDirection][defensiveCurrentZinYDirection] = false;
 						return;
 					}
-					
-						
+
+
 				}
 				else{
-					if(b.place(offensiveStartXinZDirection, offensiveStartYinZDirection, getPlayer())){
-						System.out.println(possibleWinZ[offensiveStartXinZDirection][offensiveStartYinZDirection]);
-						possibleWinZ[offensiveStartXinZDirection][offensiveStartYinZDirection] = false;
+					if(b.place(defensiveStartXinZDirection, defensiveStartYinZDirection, getPlayer())){
+						System.out.println(defensivePossibleWinZ[defensiveStartXinZDirection][defensiveStartYinZDirection]);
+						defensivePossibleWinZ[defensiveStartXinZDirection][defensiveStartYinZDirection] = false;
 						return;
 					}
-					
+
 				}
 			}
 		}
-		
-		/* =========================================================================== //
+		else{
+
+			/* =========================================================================== //
 					Where the AI should put the balls, offensive calculations
-		// =========================================================================== */
-		
+		// =========================================================================== */ 
+			if(!(offensiveMaxBallsInARowX == 0 && offensiveMaxBallsInARowY ==0 && offensiveMaxBallsInaRowZ < 2)){
+				if(offensiveMaxBallsInARowX >= offensiveMaxBallsInARowY){
+					if(offensiveMaxBallsInARowX >= offensiveMaxBallsInaRowZ){
+						System.out.println("printar i x offensive " + count);
+						int xcurrenty = 0;
+						for(int y = 0; y < b.getColumns(); y++){
+							if(count != 1){
+								if(b.peek(offensiveStartXinXDirection,y,offensiveCurrentZinXDirection) == null){
+									b.place(offensiveStartXinXDirection, y, getPlayer());
+									hasPlaced = true;
+									xcurrenty = y;
+									break;
+								}
+							}
+							else{
+								offensiveStartXinXDirection = (int)(Math.random()*b.getRows());
+								xcurrenty = (int)(Math.random()*b.getColumns());
+								System.out.println(offensiveStartXinXDirection + ", " + xcurrenty+", "+ b.getColumns());
+								b.place(offensiveStartXinXDirection, xcurrenty, getPlayer());
+								hasPlaced = true;
+								break;
+							}
+						}
+
+						if(hasPlaced){
+							for(int y = 0; y<b.getColumns();y++){
+								defensivePossibleWinX[offensiveStartXinXDirection][y][offensiveCurrentZinXDirection]= false;
+								defensivePossibleWinY[y][xcurrenty][offensiveCurrentZinXDirection] = false;
+							}
+							defensivePossibleWinZ[offensiveStartXinXDirection][offensiveCurrentZinXDirection] = false;
+							return;
+						}
+					}
+					else{
+						if(b.place(offensiveStartXinZDirection, offensiveStartYinZDirection, getPlayer())){
+							System.out.println(offensivePossibleWinZ[offensiveStartYinYDirection][offensiveCurrentZinYDirection]);
+							defensivePossibleWinZ[offensiveStartXinZDirection][offensiveStartYinZDirection] = false;
+							return;
+						}
+					}
+				}
+				else{
+					if(offensiveMaxBallsInARowY >= offensiveMaxBallsInaRowZ){
+						System.out.println("printar i y, " + defensiveMaxBallsInARowY);
+						int ycurrentx = 0;
+						for(int x = 0; x < b.getRows(); x++){
+							if(b.peek(x,offensiveStartYinYDirection,offensiveCurrentZinYDirection) == null){					
+								b.place(x, offensiveStartYinYDirection, getPlayer());
+								hasPlaced = true;
+								ycurrentx = x;
+								break;	
+							}
+						}
+						if(hasPlaced){
+							for (int x = 0; x < b.getRows(); x++) {
+								defensivePossibleWinY[x][offensiveStartYinYDirection][offensiveCurrentZinYDirection]= false;
+								defensivePossibleWinX[ycurrentx][x][offensiveCurrentZinYDirection] = false;
+							}
+							defensivePossibleWinZ[offensiveStartYinYDirection][offensiveCurrentZinYDirection] = false;
+							return;
+						}
+
+
+					}
+					else{
+						if(b.place(offensiveStartXinZDirection, offensiveStartYinZDirection, getPlayer())){
+							System.out.println(offensivePossibleWinZ[offensiveStartXinZDirection][offensiveStartYinZDirection]);
+							defensivePossibleWinZ[offensiveStartXinZDirection][offensiveStartYinZDirection] = false;
+							return;
+						}
+
+					}
+				}
+			}
+		}
+
+
+
 		while(true){
 			System.out.println("AI goes Random!");
 			int x = (int) (b.getRows() * Math.random());
@@ -259,13 +429,23 @@ public class AIUser extends User {
 				return;				
 			}
 		}
-		
+
+	}
+
+	public int countHeight(Board board, int row, int column){
+		int calculateHeight = 0;
+		while(true){
+			if(board.peek(row, column, calculateHeight) == null)
+				break;
+			calculateHeight++;
+		}
+		return calculateHeight;
 	}
 
 	public String getNotice() {
 		return "Please wait. The AI is working.";
 	}
-	
+
 	public void disconnect() {}
 
 }
